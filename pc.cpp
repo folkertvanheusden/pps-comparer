942fe34e84556b07d6365e31dca9465d2a40f0a6
Folkert van Heusden <mail@vanheusden.com>
2024-07-23 20:54:28 +0200

    can retrieve values

--- pc.cpp
+++ pc.cpp
@@ -2,7 +2,9 @@
 #include <cstdlib>
 #include <cstdio>
 #include <cstring>
+#include <ctime>
 #include <fcntl.h>
+#include <thread>
 #include <sys/timepps.h>
 
 
@@ -35,35 +37,33 @@ pps_handle_t open_pps(const char *const filename)
 	return handle;
 }
 
-int main(int argc, char *argv[])
+void get_pps(const char *const filename)
 {
-	pps_handle_t handle1 = open_pps(argv[1]);
-	pps_handle_t handle2 = open_pps(argv[2]);
+	pps_handle_t handle = open_pps(filename);
 
 	for(;;) {
-		pps_info_t infobuf1 { };
-		if (time_pps_fetch(handle1, PPS_TSFMT_TSPEC, &infobuf1, nullptr) == -1) {
-			fprintf(stderr, "Cannot time_pps_fetch from %s: %s\n", argv[1], strerror(errno));
+		pps_info_t infobuf { };
+		if (time_pps_fetch(handle, PPS_TSFMT_TSPEC, &infobuf, nullptr) == -1) {
+			fprintf(stderr, "Cannot time_pps_fetch from %s: %s\n", filename, strerror(errno));
 			exit(1);
 		}
 
-		timespec ts { 0, 100000000 };  // 100 ms sanity check
-		pps_info_t infobuf2 { };
-		if (time_pps_fetch(handle2, PPS_TSFMT_TSPEC, &infobuf2, &ts) == -1) {
-			fprintf(stderr, "Cannot time_pps_fetch from %s: %s\n", argv[2], strerror(errno));
-			continue;
-		}
-
-		printf("Assert timestamp 1: %d.%09d, sequence: %ld\n",
-				infobuf1.assert_timestamp.tv_sec,
-				infobuf1.assert_timestamp.tv_nsec,
-				infobuf1.assert_sequence);
-
-		printf("Assert timestamp 2: %d.%09d, sequence: %ld\n",
-				infobuf2.assert_timestamp.tv_sec,
-				infobuf2.assert_timestamp.tv_nsec,
-				infobuf2.assert_sequence);
+		printf("Assert timestamp from %s: %d.%09d, sequence: %ld\n",
+				filename,
+				infobuf.assert_timestamp.tv_sec,
+				infobuf.assert_timestamp.tv_nsec,
+				infobuf.assert_sequence);
 	}
+}
+
+int main(int argc, char *argv[])
+{
+	std::thread th1(get_pps, argv[1]);
+
+	std::thread th2(get_pps, argv[2]);
+
+	for(;;)
+		sleep(1);
 
 	return 0;
 }
